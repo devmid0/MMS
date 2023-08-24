94dab2837dbb539b87f3279904988bdafb772af5
devmid0 <ahmedessamelalfey1962@gmail.com>
2023-08-24 18:49:48 +0300

    hello 3nd

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-function divideAmount() {
+function handleButtonClick() {
     const amount = parseFloat(document.getElementById("amount").value);
-    const share = (amount / 3).toFixed(2);
 
-    document.getElementById("personalShare").textContent = parseFloat(share) !== 0 ? share : "-";
-    document.getElementById("investmentsShare").textContent = parseFloat(share) !== 0 ? share : "-";
-    document.getElementById("liabilitiesShare").textContent = parseFloat(share) !== 0 ? share : "-";
+    if (isNaN(amount) || amount < 0) {
+        return;
+    }
+
+    const total = Math.floor(amount * 0.3);
+    const charity = Math.floor(amount * 0.1);
+    const remainingAmount = amount - ((total + charity) * 3);
+
+    document.getElementById("liabilities").textContent = total + " جنية";
+    document.getElementById("investments").textContent = total + " جنية";
+    document.getElementById("personal").textContent = total + " جنية";
+    document.getElementById("charity").textContent = charity + " جنية";
+
+    const button = document.getElementById("divideButton");
+    button.disabled = true;
+
+    document.getElementById("amount").value = "";
 }
 
-document.addEventListener("keydown", function(event) {
+document.getElementById("amount").addEventListener("input", function () {
+    const button = document.getElementById("divideButton");
+    button.disabled = isNaN(parseFloat(this.value));
+});
+
+document.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-        divideAmount();
+        handleButtonClick();
     }
 });
